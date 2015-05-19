c739c6b4db44621bc9a524b8e1f9a5c5aff8d92f
Tom Oinn <tomoinn@crypticsquid.com>
2015-05-19 16:51:21 +0100

    Done some investigation on how to get at the element being injected when using a viewmodel, using filerecord-table as a rather spurious example. Dominic - see comments to work out how to do charting viewmodels...

--- src/cameraWebsite/src/components/filerecord-table/filerecord-table.js
+++ src/cameraWebsite/src/components/filerecord-table/filerecord-table.js
@@ -4,6 +4,19 @@ define(['knockout', 'text!./filerecord-table.html'], function (ko, templateMarku
         this.fileRecords = params.fileRecords;
     }
 
-    return {viewModel: FileRecordTable, template: templateMarkup};
+    return {
+        viewModel: {
+            createViewModel: function (params, componentInfo) {
+                console.log(componentInfo);
+                /*
+                 Can retrieve the element with componentInfo.element here, so, if this were a graph component
+                 of some kind rather than a simple table, we could build a new Canvas element and inject it here
+                 to use as the contents of the view. By manually subscribing to the observables defined by the view
+                 model we could pick up changes and use them to redraw the canvas appropriately. Over to you, Dominic...
+                 */
+                return new FileRecordTable(params);
+            }
+        }, template: templateMarkup
+    };
 
 });
