7969e258fadb74e351011d01fa3fa694e2157d1b
Dominic Ford <dcf21-github@dcford.org.uk>
2015-05-21 18:06:55 +0000

    Increased H264 bitrate recorded by cameras to 4Mbit/s. We were getting hideous artifacts at 1Mbit/s

--- src/videoProcess/src/converters/rawvid2mp4_libav.c
+++ src/videoProcess/src/converters/rawvid2mp4_libav.c
@@ -81,7 +81,7 @@ int main(int argc, char **argv)
   ctxEncode = video_avstream->codec;
 
   /* put sample parameters */
-  ctxEncode->bit_rate = 10000*1000;
+  ctxEncode->bit_rate = 4000*1000;
   ctxEncode->bit_rate_tolerance = 0;
   ctxEncode->rc_max_rate = 0;
   ctxEncode->rc_buffer_size = 0;
