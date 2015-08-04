3e431b409145ccd24da9cebdce3fe9d7ae9972f6
Dominic Ford <dcf21-github@dcford.org.uk>
2015-08-04 18:04:46 +0100

    Added /api to export url

--- installation_info.py
+++ installation_info.py
@@ -5,7 +5,7 @@ LONGITUDE   = 0.147193 # Degrees east
 CAMERA_NAME = "Cambridge-Ford"
 
 # Configure export of data to a remote server
-EXPORT_URL      = "https://meteorpi.cambridgesciencecentre.org"
+EXPORT_URL      = "https://meteorpi.cambridgesciencecentre.org/api"
 EXPORT_USERNAME = "" # The username used to log in to the remote server
 EXPORT_PASSWORD = "" # Corresponding password
 
