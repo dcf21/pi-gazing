22fed0e94934b561ed6c3741e9147d9e9d21b946
Dominic Ford <dcf21-git@dcford.org.uk>
2015-11-22 00:07:35 +0000

    Added script for producing camera status report

---
+++ src/cameraControl/cameraStatusLog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# cameraStatusLog.py
+
+cd /home/pi/meteor-pi/src/cameraControl/
+echo -e "\n\n# Disk usage"
+df
+
+echo -e "\n\n# File checksums"
+find -type f -exec md5sum "{}" +
+
+echo -e "\n\n# Log messages"
+tail -n 1000 ../../datadir/meteorPi.log 
+
