6cc8ca9d8f84c16f1c552c2b5eac125b0456d7a0
Dominic Ford <dcf21-github@dcford.org.uk>
2015-11-25 22:07:05 +0000

    added setting dataLocalLifetime

--- installation_info.py
+++ installation_info.py
@@ -1,15 +1,17 @@
 # In this file you must configure the default position and name of this camera
 
-LATITUDE    = 52.194317 # Degrees north
-LONGITUDE   = 0.147193 # Degrees east
-CAMERA_NAME = "Cambridge-Ford"
+LATITUDE = 52.194317  # Degrees north
+LONGITUDE = 0.147193  # Degrees east
+CAMERA_NAME = "Cambridge-South-East"
 
 # This flag sets the polarity of the relay used to turn the camera on
 # GPIO line 12 is set to this state to turn the camera ON
 relayOnGPIOState = True
 
-# Configure export of data to a remote server
-EXPORT_URL      = "https://meteorpi.cambridgesciencecentre.org/api/import"
-EXPORT_USERNAME = "" # The username used to log in to the remote server
-EXPORT_PASSWORD = "" # Corresponding password
+# This flag sets how long we keep data locally on the SD card for (days)
+dataLocalLifetime = 5
 
+# Configure export of data to a remote server
+EXPORT_URL = "https://meteorpi.cambridgesciencecentre.org/api/import"
+EXPORT_USERNAME = ""  # The username used to log in to the remote server
+EXPORT_PASSWORD = ""  # Corresponding password
