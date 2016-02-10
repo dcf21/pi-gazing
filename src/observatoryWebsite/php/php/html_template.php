d77320cfbfc89187d2e227b158756709e0862d3e
Dominic Ford <dcf21-git@dcford.org.uk>
2016-02-10 15:50:25 +0000

    Added favicons

--- src/observatoryWebsite/php/php/html_template.php
+++ src/observatoryWebsite/php/php/html_template.php
@@ -103,6 +103,10 @@ __HTML__;
                   content="shooting stars, meteors, camera, night sky"/>
             <meta name="generator" content="Dominic Ford"/>
             <meta name="author" content="Dominic Ford"/>
+            <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+            <link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
+            <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+            <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
             <meta name="viewport" content="width=device-width, initial-scale=1">
 
             <title id="title1">
