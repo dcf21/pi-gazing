b454b932ed71d11079db608c2def76c8cdc4f956
Dominic Ford <dcf21-github@dcford.org.uk>
2016-02-13 05:29:31 +0000

    Added javascript for sequential help panes

---
+++ src/observatoryWebsite/php/js/paneSequence.js
@@ -0,0 +1,88 @@
+// paneSequence.js
+// Meteor Pi, Cambridge Science Centre
+// Dominic Ford
+
+// This class implements a sequence of panes, as on the what to do page
+
+// Module for sequential panels
+function SequentialPanes(parent) {
+    var self = this;
+    var i;
+    this.parent = parent;
+    this.children = $(".pane-item", parent);
+    this.number = this.children.length;
+    this.current = 0;
+
+    this.titles = this.children.map(function () {
+        return $(this).data("title")
+    }).get();
+
+    this.contents = "<ul>";
+    for (i = 0; i < this.number; i++)
+        this.contents += "<li><a class='goto" + String(i) + "'>" + this.titles[i] + "</a></li>";
+    this.contents += "</ul>";
+    $(".pane-list", parent).html(this.contents);
+
+    $(".pane-controls", parent).html(
+        '<div style="display:inline-block;padding:10px;">' +
+        '<button type="button" class="spprev btn">Back</button>' +
+        '</div><div style="display:inline-block;padding:10px;">' +
+        '<button type="button" class="spnext btn btn-primary">Next</button>' +
+        '</div>');
+    $(".spprev", parent).click(function () {
+        self.prev();
+    });
+    $(".spnext", parent).click(function () {
+        self.next();
+    });
+    for (i = 0; i < this.number; i++) {
+        $(".goto" + String(i), parent).click(self.gotoPane(self, i));
+    }
+    this.selectPane(this.current);
+}
+
+SequentialPanes.prototype.prev = function () {
+    if (this.current > 0) this.selectPane(this.current - 1);
+};
+
+SequentialPanes.prototype.next = function () {
+    if (this.current < this.number - 1) this.selectPane(this.current + 1);
+};
+
+// This is a hack to retain the values of x and i in a closure
+SequentialPanes.prototype.gotoPane = function (x, i) {
+    return function() { x.selectPane(i); }
+}
+
+SequentialPanes.prototype.selectPane = function (i) {
+    var self = this;
+    var j = this.children[this.current];
+    var k = this.children[i];
+    this.children.each(function (l, el) {
+        $(el).stop();
+        if ((l != i) && (l != self.current)) $(el).css('display', 'none');
+    });
+
+    // Change panes with a fade effect
+    $(j).fadeOut(400, function () {
+        $(k).fadeIn(400);
+    });
+    this.current=i;
+
+    // Disable or enable previous / next buttons as required
+    if (i==0) $(".spprev", self.parent).prop("disabled",true);
+    else $(".spprev", self.parent).prop("disabled",false);
+    if (i==this.number-1) $(".spnext", self.parent).prop("disabled",true);
+    else $(".spnext", self.parent).prop("disabled",false);
+};
+
+// Initialise all HTML elements with class pane-sequence
+function sequentialPanesRegister() {
+    $(".pane-sequence").each(function (i, el) {
+        var elj = $(el);
+        var slider = new SequentialPanes(elj);
+        elj.data("handler", slider);
+    });
+}
+
+$(sequentialPanesRegister);
--- src/observatoryWebsite/php/whattodo.php
+++ src/observatoryWebsite/php/whattodo.php
@@ -20,122 +20,165 @@ $pageInfo = [
 $pageTemplate->header($pageInfo);
 
 ?>
-    <h3>Browsing our data online</h3>
-    <p class="text">
-        To search for images collected by Meteor Pi cameras, click on the "Search the Sky" tab at the top of this page.
-    </p>
+    <h3>Getting started with Meteor Pi</h3>
 
-    <h3>Is it a bird... is it a plane?</h3>
+    <div class="pane-sequence">
+        <div class="row">
+            <div class="col-md-9">
+                <div style="padding:3px;">
+                    <div class="grey_box" style="padding:16px;">
+                        <div class="pane-item" data-title="Searching the sky">
+                            <p class="text">
+                                To search for images collected by Meteor Pi cameras, click on the "Search the Sky" tab
+                                at the
+                                top of this page.
+                            </p>
+                            <div class="pane-controls"></div>
+                        </div>
 
-    <p class="text">
-        Not all the videos contain real moving objects. Sometimes the cameras trigger because of twinkling stars or
-        video
-        artifacts.
-        Look through until you find a video that shows a moving object. The next challenge is to work out what it
-        is.
-        Meteor Pi cameras pick up large numbers of satellites and planes, as well as meteors.
-    </p>
+                        <div class="pane-item" data-title="What can I see?">
+                            <h3>Is it a bird... is it a plane?</h3>
 
-    <div class="row">
-        <div class="col-md-6">
-            <h4>Is it a meteor?</h4>
+                            <p class="text">
+                                Not all the videos contain real moving objects. Sometimes the cameras trigger because of
+                                twinkling stars or
+                                video
+                                artifacts.
+                                Look through until you find a video that shows a moving object. The next challenge is to
+                                work
+                                out what it
+                                is.
+                                Meteor Pi cameras pick up large numbers of satellites and planes, as well as meteors.
+                            </p>
+                            <div class="pane-controls"></div>
+                        </div>
 
-            <p class="text">
-                Meteors are distinctive, because they move much more quickly than planes and satellites, at speeds of over
-                10
-                kilometres each second. They are
-                usually only visible for a fraction of a second as they burn up. To find more meteors, you may want change
-                the "Max
-                duration" field to one second and repeat your search.
-            </p>
+                        <div class="pane-item" data-title="Spotting meteors">
+                            <h4>Is it a meteor?</h4>
 
-        </div>
-        <div class="col-md-6">
+                            <p class="text">
+                                Meteors are distinctive, because they move much more quickly than planes and satellites,
+                                at
+                                speeds of over
+                                10
+                                kilometres each second. They are
+                                usually only visible for a fraction of a second as they burn up. To find more meteors,
+                                you may
+                                want change
+                                the "Max
+                                duration" field to one second and repeat your search.
+                            </p>
 
-            <div style="padding:4px;margin:8px;border:1px solid #888;width:450px;box-sizing: content-box;">
-                <img style="width:450px;"
-                     src="https://meteorpi.cambridgesciencecentre.org/api/files/content/00eba2af11f5452cba1ee177c1cc4ebe/20151107213909_Bury-Allman_event_maxBrightness_LC1.png"/>
-                <p class="caption">An example image of a meteor, as it might appear when you search our website.<a
-                        href="https://meteorpi.cambridgesciencecentre.org/#file/%257B%2522camera_ids%2522%253A%2522Bury-St-Edmunds%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2522%252C%2522searchtype%2522%253A%2522Moving%2520objects%2522%252C%2522before%2522%253A1446932350000%252C%2522after%2522%253A1446932348000%257D">
-                        Click here to see a full video of the event.
-                    </a></p>
+                            <div class="rightimg">
+                                <img style="width:450px;"
+                                     src="https://meteorpi.cambridgesciencecentre.org/api/files/content/00eba2af11f5452cba1ee177c1cc4ebe/20151107213909_Bury-Allman_event_maxBrightness_LC1.png"/>
+                                <p class="caption">An example image of a meteor, as it might appear when you search our
+                                    website.<a
+                                        href="https://meteorpi.cambridgesciencecentre.org/#file/%257B%2522camera_ids%2522%253A%2522Bury-St-Edmunds%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2522%252C%2522searchtype%2522%253A%2522Moving%2520objects%2522%252C%2522before%2522%253A1446932350000%252C%2522after%2522%253A1446932348000%257D">
+                                        Click here to see a full video of the event.
+                                    </a></p>
+                            </div>
+                            <div class="pane-controls"></div>
+                        </div>
+
+                        <div class="pane-item" data-title="Spotting planes">
+                            <h4>Is it a plane?</h4>
+
+                            <p class="text">
+                                Planes are often easy to spot because they have flashing lights. They are usually
+                                visible for
+                                10-30 seconds,
+                                whereas
+                                meteors are gone in a fraction of a second. If you set the "Min duration" field to 10 or
+                                20
+                                seconds, most of
+                                the
+                                objects you'll see will be planes.
+                            </p>
+
+                            <div class="rightimg">
+                                <img style="width:450px;"
+                                     src="https://meteorpi.cambridgesciencecentre.org/api/files/content/00bcb31ce40e4b4ca2db1f25e73e22d1/20151109183940_Bury-Allman_event_maxBrightness_LC1.png"/>
+                                <p class="caption">An example image of a plane, as it might appear when you search our
+                                    website.<a
+                                        href="https://meteorpi.cambridgesciencecentre.org/#file/%257B%2522camera_ids%2522%253A%2522Bury-St-Edmunds%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2522%252C%2522searchtype%2522%253A%2522Moving%2520objects%2522%252C%2522before%2522%253A1446932350000%252C%2522after%2522%253A1446932348000%257D">
+                                        Click here to see a full video of the event.
+                                    </a></p>
+                            </div>
+                            <div class="pane-controls"></div>
+                        </div>
+
+                        <div class="pane-item" data-title="Spotting satellites">
+                            <h4>Is it satellite?</h4>
+
+                            <p class="text">
+                                Satellites, like planes, are visible for much longer than meteors &ndash; at least 10-30
+                                seconds. They move
+                                at a
+                                steady speed and they don't have flashing lights.
+                            </p>
+
+                            <p class="text">
+                                The flight paths of satellites are extremely predictable, and so if you think you've
+                                found one,
+                                you can try
+                                looking
+                                on a website like <a href="https://in-the-sky.org/satpasses.php">In-The-Sky.org</a> to
+                                see if
+                                you can track
+                                down
+                                which satellite it was.
+                            </p>
+
+                            <p class="text">
+                                Alternatively, why not look up which satellites flew over the UK last night, and see if
+                                any
+                                Meteor Pi
+                                cameras picked
+                                them up?
+                            </p>
+
+                            <div class="rightimg">
+                                <img style="width:450px;"
+                                     src="https://meteorpi.cambridgesciencecentre.org/api/files/content/3f5568248f264477a4fec4f45f8dacda/20151010182540_Bury-Allman_event_maxBrightness_LC1.png"/>
+                                <p class="caption">An example image of a satellite, as it might appear when you search
+                                    our
+                                    website.<a
+                                        href="https://meteorpi.cambridgesciencecentre.org/#file/%257B%2522camera_ids%2522%253A%2522Bury-St-Edmunds%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2522%252C%2522searchtype%2522%253A%2522Moving%2520objects%2522%252C%2522before%2522%253A1444501541000%252C%2522after%2522%253A1444501539000%257D">
+                                        Click here to see a full video of the event.
+                                    </a></p>
+                            </div>
+                            <div class="pane-controls"></div>
+                        </div>
+
+                        <div class="pane-item" data-title="Where next?">
+
+                            <h3 style="clear:both;">What next?</h3>
+
+                            <p class="text">
+                                Once you've started looking at Meteor Pi images, you'll want to know what else you can
+                                see!
+                            </p>
+
+                            <p class="text">
+                                Why not take a look at <a href="/projects">our projects</a>, to see what else you can
+                                see?
+                            </p>
+                            <div class="pane-controls"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div style="padding:3px;">
+                    <div class="grey_box" style="padding:16px;">
+                        <h4 style="padding-top:0;">Contents</h4>
+                        <div class="pane-list"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-md-6">
-            <h4>Is it a plane?</h4>
-
-            <p class="text">
-                Planes are often easy to spot because they have flashing lights. They are usually visible for 10-30 seconds,
-                whereas
-                meteors are gone in a fraction of a second. If you set the "Min duration" field to 10 or 20 seconds, most of
-                the
-                objects you'll see will be planes.
-            </p>
-
-        </div>
-        <div class="col-md-6">
-
-            <div style="padding:4px;margin:8px;border:1px solid #888;width:450px;box-sizing: content-box;">
-                <img style="width:450px;"
-                     src="https://meteorpi.cambridgesciencecentre.org/api/files/content/00bcb31ce40e4b4ca2db1f25e73e22d1/20151109183940_Bury-Allman_event_maxBrightness_LC1.png"/>
-                <p class="caption">An example image of a plane, as it might appear when you search our website.<a
-                        href="https://meteorpi.cambridgesciencecentre.org/#file/%257B%2522camera_ids%2522%253A%2522Bury-St-Edmunds%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2522%252C%2522searchtype%2522%253A%2522Moving%2520objects%2522%252C%2522before%2522%253A1446932350000%252C%2522after%2522%253A1446932348000%257D">
-                        Click here to see a full video of the event.
-                    </a></p>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-6">
-            <h4>Is it satellite?</h4>
-
-            <p class="text">
-                Satellites, like planes, are visible for much longer than meteors &ndash; at least 10-30 seconds. They move
-                at a
-                steady speed and they don't have flashing lights.
-            </p>
-
-            <p class="text">
-                The flight paths of satellites are extremely predictable, and so if you think you've found one, you can try
-                looking
-                on a website like <a href="https://in-the-sky.org/satpasses.php">In-The-Sky.org</a> to see if you can track
-                down
-                which satellite it was.
-            </p>
-
-            <p class="text">
-                Alternatively, why not look up which satellites flew over the UK last night, and see if any Meteor Pi
-                cameras picked
-                them up?
-            </p>
-
-        </div>
-        <div class="col-md-6">
-
-            <div style="padding:4px;margin:8px;border:1px solid #888;width:450px;box-sizing: content-box;">
-                <img style="width:450px;"
-                     src="https://meteorpi.cambridgesciencecentre.org/api/files/content/3f5568248f264477a4fec4f45f8dacda/20151010182540_Bury-Allman_event_maxBrightness_LC1.png"/>
-                <p class="caption">An example image of a satellite, as it might appear when you search our website.<a
-                        href="https://meteorpi.cambridgesciencecentre.org/#file/%257B%2522camera_ids%2522%253A%2522Bury-St-Edmunds%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2522%252C%2522searchtype%2522%253A%2522Moving%2520objects%2522%252C%2522before%2522%253A1444501541000%252C%2522after%2522%253A1444501539000%257D">
-                        Click here to see a full video of the event.
-                    </a></p>
-            </div>
-        </div>
-    </div>
-
-    <h3 style="clear:both;">What next?</h3>
-
-    <p class="text">
-        Once you've started looking at Meteor Pi images, you'll want to know what else you can see!
-    </p>
-
-    <p class="text">
-        Why not take a look at <a href="/projects">our projects</a>, to see what else you can see?
-    </p>
 
 <?php
 $pageTemplate->footer($pageInfo);
