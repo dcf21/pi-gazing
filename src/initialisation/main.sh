1a6ed37c889afb92c9bc947531ebf8f116e59d1d
Dominic Ford <dcf21-git@dcford.org.uk>
2019-06-09 01:10:07 +0100

    Initialisation should include building C code

--- src/initialisation/main.sh
+++ src/initialisation/main.sh
@@ -9,16 +9,23 @@ read -p $'This will destroy and rebuild the Pi Gazing databases.\nHit "Y" to con
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "1/6: Creating database and associated user in MySQL. You will need to enter your MySQL root password."
+    echo "1/7: Creating database and associated user in MySQL. You will need to enter your MySQL root password."
     sudo mysql -u root -p < createDatabase.sql
 
-    echo "2/6: Creating MySQL configuration file, so that we don't have to pass passwords on the command line."
+    echo "2/7: Building the C code."
+    cd ../observe/video_analysis/
+    ./prettymake
+    cd ../../helpers/image_processing/
+    ./prettymake
+    cd ${DIR}
+
+    echo "3/7: Creating MySQL configuration file, so that we don't have to pass passwords on the command line."
     ./makeMysqlConfig.py
 
-    echo "3/6: Setting up database schema."
+    echo "4/7: Setting up database schema."
     sudo mysql --defaults-extra-file=../../datadir/mysql_login.cfg < databaseSchema.sql
 
-    echo "4/6: Creating directory to store files associated with database."
+    echo "5/7: Creating directory to store files associated with database."
     mkdir -p ../../datadir/raw_video
     mkdir -p ../../datadir/db_filestore
     mkdir -p ../../datadir/thumbnails
@@ -26,10 +33,10 @@ then
     sudo rm -f ../../datadir/db_filestore/*
     sudo rm -f ../../datadir/thumbnails/*
 
-    echo "5/6: Populating table of constellations."
+    echo "6/7: Populating table of constellations."
     ./populateConstellations.py
 
-    echo "6/6: Set up default exports."
+    echo "7/7: Set up default exports."
     ./defaultExports.py
 else
     echo "Operation cancelled, no changes made."
